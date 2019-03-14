1f470e0ef26f2678fabcae39c3f54e0b9c410a23
Manoj Behera <manoj.behera@mavenir.com>
2019-03-14 13:12:39 +0530

    Initial commit

---
+++ app/src/main/java/com/idigita/textviewoverimageview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.idigita.textviewoverimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
